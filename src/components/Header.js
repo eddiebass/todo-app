a617655497fefc37edab0b722f223e8eeb3b50ca
Eddie Bassey <eddie.bassey@nology.io>
2022-05-18 20:06:45 +0100

    added date and applied css to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Header = () => {
-	return <div>Header</div>;
+	const date = new Date();
+	const theDate = date.getDate();
+	const month = () => {
+		const options = { month: "long" };
+		return new Intl.DateTimeFormat("en-US", options).format(date);
+	};
+	const day = () => {
+		const options = { weekday: "long" };
+		return new Intl.DateTimeFormat("en-US", options).format(date);
+	};
+	return (
+		<header className="border">
+			<div className="head-list">
+				<h1>
+					{`${day()}, ${theDate}`}
+					<sup>th</sup>
+				</h1>
+				<h2>0 Task</h2>
+			</div>
+			<p className="month">{month()}</p>
+		</header>
+	);
 };
 
 export default Header;
