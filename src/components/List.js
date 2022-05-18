59e3135b9ab1d45bb395c4ce5fbd162dd6287087
Eddie Bassey <eddie.bassey@nology.io>
2022-05-19 00:22:09 +0100

    styled List component and installed react icon

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BsCheckSquareFill, BsCheckSquare } from "react-icons/bs";
 
 const List = () => {
-	return <div>List</div>;
+	return (
+		<div>
+			<article className="border">
+				<section>
+					<div className="tick-box">
+						<BsCheckSquareFill className="tick" />
+					</div>
+					<p>Going to the Park with Anne</p>
+				</section>
+				<h3>...</h3>
+			</article>
+			<article className="border">
+				<section>
+					<div className="tick-box">
+						<BsCheckSquareFill className="tick" />
+					</div>
+					<p>Renewing my broadband subscription</p>
+				</section>
+				<h3>...</h3>
+			</article>
+		</div>
+	);
 };
 
 export default List;
